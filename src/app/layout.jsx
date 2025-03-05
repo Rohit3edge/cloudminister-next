e021009894c78ad33935a4ac884748577de29149
Rohit3edge <rohit@3edgetechnologies.com>
2025-03-05 11:30:20 +0530

    New Upadate in favicon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
-
-// import { useEffect } from "react";
+"use client";
+import { useState, useEffect } from "react";
+import { usePathname } from "next/navigation"; 
 import { Plus_Jakarta_Sans, Public_Sans } from "next/font/google";
 import { Providers } from "./providers";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -9,7 +10,7 @@ import "../../public/assets/css/vendors/swiper-bundle.min.css";
 import "line-awesome/dist/line-awesome/css/line-awesome.min.css";
 import Layout from "@/components/common/layout/Layout";
 import ContactButtons from "@/components/common/others/ContactButtons";
-// import ChatWidget from "@/components/common/others/ChatWidget";
+import ChatWidget from "@/components/common/others/ChatWidget";
 
 const plusJakarta = Plus_Jakarta_Sans({
   variable: "--font-plus-jakarta",
@@ -23,27 +24,34 @@ const publicSans = Public_Sans({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
-export const metadata = {
-  title: "CloudMinister | Reliable Web, Cloud Hosting & Server Management Solutions in India",
-  description:
-    "CloudMinister offers secure, high-performance Web hosting, VPS, Dedicated Server, cloud hosting, server management, and IT solutions. Get 24/7 support and scalable cloud infrastructure for your business.",
-};
+function Loader() {
+  return (
+    <div className="loader-container">
+      <img src="/img/logo-light.webp" alt="Loading..." className="loader-logo" />
+    </div>
+  );
+}
 
 export default function RootLayout({ children }) {
-  // useEffect(() => {
-  //   import("bootstrap/dist/js/bootstrap.bundle.min.js").then(() => {
-  //     console.log("Bootstrap JS loaded");
-  //   });
-  // }, []);
+  const [loading, setLoading] = useState(false);
+  const pathname = usePathname(); // Get current route
+
+  useEffect(() => {
+    setLoading(true);
+    const timer = setTimeout(() => setLoading(false), 500); // Simulate loading time
+
+    return () => clearTimeout(timer); // Cleanup
+  }, [pathname]); // Runs when the route changes
 
   return (
     <html lang="en">
       <body className={`${plusJakarta.variable} ${publicSans.variable}`}>
+        {loading && <Loader />}
         <Providers>
           <Layout>
-            <ContactButtons/>
-            {/* <ChatWidget/> */}
-          {children}
+            <ContactButtons />
+            <ChatWidget />
+            {children}
           </Layout>
         </Providers>
       </body>
